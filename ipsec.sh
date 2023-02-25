cfa65967ef986d4a2798ec6d038e6e532294c52e
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-02-25 21:53:59 +0200

    Update ipsec.sh

--- ipsec.sh
+++ ipsec.sh
@@ -20,7 +20,7 @@ IP=$(wget -qO- ipinfo.io/ip);
 echo -e "checking vps"
 # ==================================================
 # Link Hosting Kalian
-wget https://raw.githubusercontent.com/Gandring15/vps/main/ipsec.sh"
+wget https://raw.githubusercontent.com/Rolka111111/vps/main/ipsec.sh"
 chmod +x ipsec
 VPN_IPSEC_PSK='gandring'
 NET_IFACE=$(ip -o $NET_IFACE -4 route show to default | awk '{print $5}');
@@ -294,12 +294,12 @@ mkdir -p /run/pluto
 service fail2ban restart 2>/dev/null
 service ipsec restart 2>/dev/null
 service xl2tpd restart 2>/dev/null
-wget -O /usr/bin/addl2tp https://raw.githubusercontent.com/Gandring15/vps/main/addl2tp.sh && chmod +x /usr/bin/addl2tp
-wget -O /usr/bin/dell2tp https://raw.githubusercontent.com/Gandring15/vps/main/dell2tp.sh && chmod +x /usr/bin/dell2tp
-wget -O /usr/bin/addpptp https://raw.githubusercontent.com/Gandring15/vps/main/addpptp.sh && chmod +x /usr/bin/addpptp
-wget -O /usr/bin/delpptp https://raw.githubusercontent.com/Gandring15/vps/main/delpptp.sh && chmod +x /usr/bin/delpptp
-wget -O /usr/bin/renewpptp https://raw.githubusercontent.com/Gandring15/vps/main/renewpptp.sh && chmod +x /usr/bin/renewpptp
-wget -O /usr/bin/renewl2tp https://raw.githubusercontent.com/Gandring15/vps/main/renewl2tp.sh && chmod +x /usr/bin/renewl2tp
+wget -O /usr/bin/addl2tp https://raw.githubusercontent.com/Rolka111111/vps/main/addl2tp.sh && chmod +x /usr/bin/addl2tp
+wget -O /usr/bin/dell2tp https://raw.githubusercontent.com/Rolka111111/vps/main/dell2tp.sh && chmod +x /usr/bin/dell2tp
+wget -O /usr/bin/addpptp https://raw.githubusercontent.com/Rolka111111/vps/main/addpptp.sh && chmod +x /usr/bin/addpptp
+wget -O /usr/bin/delpptp https://raw.githubusercontent.com/Rolka111111/vps/main/delpptp.sh && chmod +x /usr/bin/delpptp
+wget -O /usr/bin/renewpptp https://raw.githubusercontent.com/Rolka111111/vps/main/renewpptp.sh && chmod +x /usr/bin/renewpptp
+wget -O /usr/bin/renewl2tp https://raw.githubusercontent.com/Rolka111111/vps/main/renewl2tp.sh && chmod +x /usr/bin/renewl2tp
 touch /var/lib/gandring/data-user-l2tp
 touch /var/lib/gandring/data-user-pptp
 rm -f /root/ipsec.sh
